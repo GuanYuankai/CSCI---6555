76b5b2145cd164d5e206753f0a195ba4aa139b1a
GuanYuankai <35901736+GuanYuankai@users.noreply.github.com>
2018-09-27 01:11:36 -0400

    Eular Control Point X-Asia

--- LAB1/Lab 1/Lab 1/main.cpp
+++ LAB1/Lab 1/Lab 1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <math.h>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -61,11 +62,21 @@ int main()
     glm::mat4 B_SplinesM = glm::make_mat4(B_splinesData);
     
    
-    float t = glfwGetTime();
+    float time1 = glfwGetTime();
+    float t = fmod(time1, 1);
     float tData[4] = { t*t*t, t*t, t, 1};
     glm::vec4 tM = glm::make_vec4(tData);
     
-  
+    float controlPointEulerX[16] =
+    {
+        0,  0,  0,  0,
+        0.25,   1.5707963,  0,  0,
+        0.75,   0,  0.7853981,  0,
+        1,  1.3264163,  0.770319,   -0.6134645
+    };
+    glm::mat4 EulerXM = glm::make_mat4(controlPointEulerX);
+    
+    
     
     
 }
