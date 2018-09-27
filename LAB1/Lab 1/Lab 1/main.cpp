51d2bf04d7dd0f83c57cc76adc00cdccc4e3a607
GuanYuankai <35901736+GuanYuankai@users.noreply.github.com>
2018-09-27 00:36:19 -0400

    2 interolate bias Matrix

--- LAB1/Lab 1/Lab 1/main.cpp
+++ LAB1/Lab 1/Lab 1/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <fstream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 using namespace std;
 
 int main()
@@ -24,15 +31,34 @@ int main()
     }
     for(i = 0; i < numberIndice * 4; i += 4)
     {
+        //can use flag to decide what type the indice is
         myfile >> flag >> indiceData1 >> indiceData2 >> indiceData3;
         indice[i] = indiceData1;
         indice[i+1] = indiceData2;
         indice[i+2] = indiceData3;
-        cout << indice[i] << " " << indice[i+1] << " " << indice[i+2] << endl;
-        
+//        cout << indice[i] << " " << indice[i+1] << " " << indice[i+2] << endl;
     }
-
+    
+    
+    float catmullRomData[16] =
+    {
+        -0.5,   1.5,  -1.5,   0.5,
+           1,  -2.5,     2,  -0.5,
+        -0.5,     0,   0.5,     0,
+           0,     1,     0,     0
+        
+    };
+    glm::mat4 catmullRomM = glm::make_mat4(catmullRomData);
  
+    float B_splinesData[16] =
+    {
+        -1.0 / 6,   3.0 / 6,   -3.0 / 6,   1.0 / 6,
+         3.0 / 6,  -6.0 / 6,    3.0 / 6,       0.0,
+        -3.0 / 6,       0.0,    3.0 / 6,       0.0,
+         1.0 / 6,   4.0 / 6,    1.0 / 6,       0.0
+    };
+    
+    glm::mat4 B_SplinesM = glm::make_mat4(B_splinesData);
     
   
     
