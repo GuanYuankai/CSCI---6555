f5096b4c6add9ec36a94ad6d6c56bf26e502d134
GuanYuankai <35901736+GuanYuankai@users.noreply.github.com>
2018-09-27 04:17:00 -0400

    crashed

--- LAB1/Lab 1/Lab 1/main.cpp
+++ LAB1/Lab 1/Lab 1/main.cpp
@@ -187,6 +187,67 @@ int main()
 
     Shader ourShader("shader.vs", "shader.fs");
     
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     
+    glBindVertexArray(VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indice), indice, GL_STATIC_DRAW);
+    cout<< sizeof(indice) << endl;
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    
+    while (!glfwWindowShouldClose(window))
+    {
+
+        processInput(window);
+        
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ourShader.use();
+        
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, numberIndice * 3, GL_UNSIGNED_INT, 0);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
 }
 
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
--- LAB1/Lab 1/Lab 1/shader.vs
+++ LAB1/Lab 1/Lab 1/shader.vs
@@ -1,9 +1,9 @@
-#version 330
+#version 330 core
 layout(location = 0) in vec3 aPos;
 
-uniform transform;
+uniform mat4 transform;
 void main()
 {
-    gl_Position = transform * vec4(aPos, 1.0);
+    gl_Position =  vec4(aPos, 1.0) / 10;
 }
 
