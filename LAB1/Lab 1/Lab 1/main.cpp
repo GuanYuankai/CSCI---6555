9818fb219e8370c0bfbdac09bfa696ffa5e5b158
GuanYuankai <35901736+GuanYuankai@users.noreply.github.com>
2018-09-26 23:51:32 -0400

    read file

--- LAB1/Lab 1/Lab 1/main.cpp
+++ LAB1/Lab 1/Lab 1/main.cpp
@@ -1,1 +1,37 @@
-#include <
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream myfile("teapot.off");
+    if(!myfile)
+    {
+        cout << "Fail to Open File" << endl;
+    }
+    
+    int numberVertex, numberIndice, zero;
+    int i = 0;
+    double indiceData;
+    myfile >> numberVertex >> numberIndice >> zero;
+    double vertex[numberVertex * 3];
+    double indice[numberIndice * 4];
+    for(i = 0; i < numberVertex * 3; i++)
+    {
+        myfile >> vertex[i];
+//        cout << vertex[i] << endl;
+    }
+    for(i = 0; i < numberIndice * 4; i++)
+    {
+        myfile >> indiceData;
+
+        }
+
+    }
+    
+  
+    
+    
+}
+
