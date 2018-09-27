41d028e7bba9cbc5f2f48cfe0d474b7f73059daa
GuanYuankai <35901736+GuanYuankai@users.noreply.github.com>
2018-09-27 14:29:20 -0400

    bug fixed and down

--- LAB1/Lab 1/Lab 1/main.cpp
+++ LAB1/Lab 1/Lab 1/main.cpp
@@ -9,6 +9,8 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/string_cast.hpp>
 #include "shader.h"
 
 using namespace std;
@@ -28,32 +30,38 @@ int main()
     
     int numberVertex, numberIndice, zero, flag;
     int i = 0;
+    int j = 0;
     double indiceData1, indiceData2, indiceData3;
     myfile >> numberVertex >> numberIndice >> zero;
-    double vertex[numberVertex * 3];
-    double indice[numberIndice * 4];
+    float vertex[numberVertex * 3];
+    int indice[numberIndice * 4];
     for(i = 0; i < numberVertex * 3; i++)
     {
         myfile >> vertex[i];
-//        cout << vertex[i] << endl;
     }
+    
+
+    
     for(i = 0; i < numberIndice * 4; i += 4)
     {
         //can use flag to decide what type the indice is
         myfile >> flag >> indiceData1 >> indiceData2 >> indiceData3;
-        indice[i] = indiceData1;
-        indice[i+1] = indiceData2;
-        indice[i+2] = indiceData3;
-//        cout << indice[i] << " " << indice[i+1] << " " << indice[i+2] << endl;
+        indice[j] = indiceData1;
+        j++;
+        indice[j] = indiceData2;
+        j++;
+        indice[j] = indiceData3;
+        j++;
+
     }
-    
-    
+
     float catmullRomData[16] =
     {
-        -0.5,   1.5,  -1.5,   0.5,
-           1,  -2.5,     2,  -0.5,
-        -0.5,     0,   0.5,     0,
-           0,     1,     0,     0
+
+        -0.5,   1,   -0.5,  0,
+        1.5,    -2.5,   0,  1,
+        -1.5,   2,  0.5,    0,
+        0.5,    -0.5,   0,  0
         
     };
     glm::mat4 catmullRomM = glm::make_mat4(catmullRomData);
@@ -71,77 +79,61 @@ int main()
    
     float time1 = glfwGetTime();
     float t = fmod(time1, 1);
+
     float tData[4] = { t*t*t, t*t, t, 1};
     glm::vec4 tM = glm::make_vec4(tData);
     
-    float controlPointEulerX[16] =
-    {   0,  0,  0,  0,
-        0.25,   0,  0,  0,
-        0.5,    0,  0,  0,
-        0.75,   0,  0,  0
-    };
+    float controlPointEulerX[4] =
+    {-0.5, -0.4, 0.5, 0.75};
     
     float controlPointEulerY[16] =
     {
-        0,      0,  0,  0,
-        0.25,   0,  0,  0,
-        0.5,    0,  0,  0,
-        0.75,   0,  0,  0
-        
+        -0.5, -0.4, 0.5, 0.75
     };
     float controlPointEulerZ[16] =
     {
-        0,      0,  0,  0,
-        0.25,   0,  0,  0,
-        0.5,    0,  0,  0,
-        0.75,   0,  0,  0
-        
+        -0.5, -0.4, 0.5, 0.75
     };
-    glm::mat4 EulerX = glm::make_mat4(controlPointEulerX);
-    glm::mat4 EulerY = glm::make_mat4(controlPointEulerY);
-    glm::mat4 EulerZ = glm::make_mat4(controlPointEulerZ);
+    glm::vec4 EulerX = glm::make_vec4(controlPointEulerX);
+    glm::vec4 EulerY = glm::make_vec4(controlPointEulerY);
+    glm::vec4 EulerZ = glm::make_vec4(controlPointEulerZ);
     
     float controlPointEulerYaw[16] =
     {
-        1.5705963,  0,  0,  0,
-        0,  0,  0,  0,
-        1.3264163,  0,  0,  0,
-        0.6154797,  0,  0,  0
-        
+        1.5705963, 0, 1.3264163, 0.6154797
     };
     float controlPointEulerPitch[16] =
     {
-        0,  0,  0,  0,
-        0.7853981,  0,  0,  0,
-        0,  0,  0,  0,
-        0.5235988,  0,  0,  0
-        
+        0, 0.7853981, 0, 0.5235988
     };
     float controlPointEulerRoll[16] =
     {
-        0,  0,  0,  0,
-        0,  0,  0,  0,
-        -0.6134645, 0,  0,  0,
-        -0.1699185, 0,  0,  0
+        0, 0, -0.6134645, -0.1699185
         
     };
-    glm::mat4 EulerYaw = glm::make_mat4(controlPointEulerYaw);
-    glm::mat4 EulerPitch = glm::make_mat4(controlPointEulerPitch);
-    glm::mat4 EulerRoll = glm::make_mat4(controlPointEulerRoll);
+    glm::vec4 EulerYaw = glm::make_vec4(controlPointEulerYaw);
+    glm::vec4 EulerPitch = glm::make_vec4(controlPointEulerPitch);
+    glm::vec4 EulerRoll = glm::make_vec4(controlPointEulerRoll);
     
-    glm::vec1 EulerCatX = tM * catmullRomM * EulerX;
-    glm::vec1 EulerCatY = tM * catmullRomM * EulerY;
-    glm::vec1 EulerCatZ = tM * catmullRomM * EulerZ;
+    glm::vec4 EulerCatX = tM * catmullRomM * EulerX;
+    float ECX = EulerCatX[0] + EulerCatX[1] + EulerCatX[2] + EulerCatX[3];
+    glm::vec4 EulerCatY = tM * catmullRomM * EulerY;
+    float ECY = EulerCatY[0] + EulerCatY[1] + EulerCatY[2] + EulerCatY[3];
+    glm::vec4 EulerCatZ = tM * catmullRomM * EulerZ;
+    float ECZ = EulerCatZ[0] + EulerCatZ[1] + EulerCatZ[2] + EulerCatZ[3];
     glm::vec1 EulerCatYaw = tM * catmullRomM * EulerYaw;
+    float ECYaw = EulerCatYaw[0] + EulerCatYaw[1] + EulerCatYaw[2] + EulerCatYaw[3];
     glm::vec1 EulerCatPitch = tM * catmullRomM * EulerPitch;
+    float ECPitch = EulerCatPitch[0] + EulerCatPitch[1] + EulerCatPitch[2] + EulerCatPitch[3];
     glm::vec1 EulerCatRoll = tM * catmullRomM * EulerRoll;
+    float ECRoll = EulerCatRoll[0] + EulerCatRoll[1] + EulerCatRoll[2] + EulerCatRoll[3];
     
-    float cx = cos(EulerCatYaw.x);
-    float sx = sin(EulerCatYaw.x);
-    float cy = cos(EulerCatPitch.x);
-    float sy = sin(EulerCatPitch.x);
-    float cz = cos(EulerCatRoll.x);
-    float sz = sin(EulerCatRoll.x);
+    float cx = cos(ECYaw);
+    float sx = sin(ECYaw);
+    float cy = cos(ECPitch);
+    float sy = sin(ECPitch);
+    float cz = cos(ECRoll);
+    float sz = sin(ECRoll);
     
     float EulerRotate[9] =
     {
@@ -153,9 +145,11 @@ int main()
     
     glm::mat4 transform(rotate);
     transform[3][3] = 1.0f;
-    transform[3][0] = EulerCatX.x;
-    transform[3][1] = EulerCatY.x;
-    transform[3][2] = EulerCatZ.x;
+    transform[3][0] = ECX;
+    transform[3][1] = ECY;
+    transform[3][2] = ECZ;
+    cout << to_string(transform) << endl;
+    
    
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -195,11 +189,11 @@ int main()
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, numberVertex * 3 * 4, vertex, GL_STATIC_DRAW);
     
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indice), indice, GL_STATIC_DRAW);
-    cout<< sizeof(indice) << endl;
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, numberIndice * 3 * 4, indice, GL_STATIC_DRAW);
+//    cout<< numberIndice * 3 * 4 << endl;
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -219,6 +213,77 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
         ourShader.use();
         
+        float time1 = glfwGetTime();
+        float t = fmod(time1, 1);
+        
+        float tData[4] = { t*t*t, t*t, t, 1};
+        glm::vec4 tM = glm::make_vec4(tData);
+        
+        float controlPointEulerX[4] =
+        {-0.5, -0.4, 0.5, 0.75};
+        
+        float controlPointEulerY[16] =
+        {
+            -0.5, -0.4, 0.5, 0.75
+        };
+        float controlPointEulerZ[16] =
+        {
+            -0.5, -0.4, 0.5, 0.75
+        };
+        glm::vec4 EulerX = glm::make_vec4(controlPointEulerX);
+        glm::vec4 EulerY = glm::make_vec4(controlPointEulerY);
+        glm::vec4 EulerZ = glm::make_vec4(controlPointEulerZ);
+        
+        float controlPointEulerYaw[16] =
+        {
+            1.5705963, 0, 1.3264163, 0.6154797
+        };
+        float controlPointEulerPitch[16] =
+        {
+            0, 0.7853981, 0, 0.5235988
+        };
+        float controlPointEulerRoll[16] =
+        {
+            0, 0, -0.6134645, -0.1699185
+            
+        };
+        glm::vec4 EulerYaw = glm::make_vec4(controlPointEulerYaw);
+        glm::vec4 EulerPitch = glm::make_vec4(controlPointEulerPitch);
+        glm::vec4 EulerRoll = glm::make_vec4(controlPointEulerRoll);
+        
+        glm::vec4 EulerCatX = tM * catmullRomM * EulerX;
+        float ECX = EulerCatX[0] + EulerCatX[1] + EulerCatX[2] + EulerCatX[3];
+        glm::vec4 EulerCatY = tM * catmullRomM * EulerY;
+        float ECY = EulerCatY[0] + EulerCatY[1] + EulerCatY[2] + EulerCatY[3];
+        glm::vec4 EulerCatZ = tM * catmullRomM * EulerZ;
+        float ECZ = EulerCatZ[0] + EulerCatZ[1] + EulerCatZ[2] + EulerCatZ[3];
+        glm::vec1 EulerCatYaw = tM * catmullRomM * EulerYaw;
+        float ECYaw = EulerCatYaw[0] + EulerCatYaw[1] + EulerCatYaw[2] + EulerCatYaw[3];
+        glm::vec1 EulerCatPitch = tM * catmullRomM * EulerPitch;
+        float ECPitch = EulerCatPitch[0] + EulerCatPitch[1] + EulerCatPitch[2] + EulerCatPitch[3];
+        glm::vec1 EulerCatRoll = tM * catmullRomM * EulerRoll;
+        float ECRoll = EulerCatRoll[0] + EulerCatRoll[1] + EulerCatRoll[2] + EulerCatRoll[3];
+        
+        float cx = cos(ECYaw);
+        float sx = sin(ECYaw);
+        float cy = cos(ECPitch);
+        float sy = sin(ECPitch);
+        float cz = cos(ECRoll);
+        float sz = sin(ECRoll);
+        
+        float EulerRotate[9] =
+        {
+            cy * cz,    cz * sx * sy - cx * sz,     sx * sz + cx * cz * sy,
+            cy * cz,    cx * cz + sx * sy * sz,     cx * sy * sz - cz * sz,
+            -sy,        cy * sx,                    cx * cy
+        };
+        glm::mat3 rotate = glm::make_mat3(EulerRotate);
+        
+        glm::mat4 transform(rotate);
+        transform[3][3] = 1.0f;
+        transform[3][0] = ECX;
+        transform[3][1] = ECY;
+        transform[3][2] = ECZ;
         unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
         glBindVertexArray(VAO);
