0d8871a6c6e71668242fc0d98c65b5b667fc7ead
GuanYuankai <35901736+GuanYuankai@users.noreply.github.com>
2018-12-17 18:54:15 -0500

    Draw a ball

--- LAB3/Lab3/Lab3/Lab3/main.cpp
+++ LAB3/Lab3/Lab3/Lab3/main.cpp
@@ -1,37 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <cmath>
+#include <cstring>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/ext.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/string_cast.hpp>
 
 #include "shader.h"
 #include "LoadModel.h"
-
 using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
-
-// settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+
 int main()
 {
+    
     string off = "ball.off";
     Model model = LoadModel(off);
+
     
+
     
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
+#ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
+#endif
     
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LAB3", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -41,51 +52,106 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     
-
+    // glad: load all OpenGL function pointers
+    
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
     
-    // render loop
-    // -----------
+    glEnable(GL_DEPTH_TEST);
+    Shader ourShader("shader.vs", "shader.fs");
+    
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, model.numberVertex * 3 * 4, model.vertex, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, model.numberIndice * 3 * 4, model.indice, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    
+ 
+    
+    
+    
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glm::mat4 view = glm::mat4(1.0f);
+    view = glm::lookAt(glm::vec3(3.0f, 3.0f, 3.0f),
+                       glm::vec3(0.0f, 0.0f, 0.0f),
+                       glm::vec3(0.0f, 1.0f, 0.0f));
+    
+    glm::mat4 projection;
+    projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH/(float)SCR_HEIGHT, 0.1f, 100.0f);
+    
+    float currentPlace = -5.0f;
+
+    
     while (!glfwWindowShouldClose(window))
     {
-        // input
-        // -----
+        
+        
+        
         processInput(window);
+        int viewLoc = glad_glGetUniformLocation(ourShader.ID, "view");
+        glad_glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        int projLoc = glad_glGetUniformLocation(ourShader.ID, "projection");
+        glad_glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        int transLoc = glad_glGetUniformLocation(ourShader.ID, "trans");
         
-        // render
-        // ------
+        
+        
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
+        ourShader.use();
+
+        
+        glm::mat4 body = glm::mat4(1.0f);
+        currentPlace = currentPlace
+        ;
+        body = glm::translate(body,glm::vec3(0.0f, 0.0f, currentPlace ));
+        glBindVertexArray(VAO);
+        glad_glUniformMatrix4fv(transLoc, 1, GL_FALSE, glm::value_ptr(body));
+        glDrawElements(GL_TRIANGLES, model.numberIndice * 3, GL_UNSIGNED_INT, 0);
+        
+
+        
+        
+        
+        
+
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
     
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
+    
     glViewport(0, 0, width, height);
 }
+
+
--- LAB3/Lab3/Lab3/Lab3/shader.fs
+++ LAB3/Lab3/Lab3/Lab3/shader.fs
@@ -4,5 +4,5 @@ out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(color * 10 , 1.0f);
+    FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 }
